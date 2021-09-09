aa8e00a54cea98d6d1eaad56f785b99f3e769aac
Robin <robin1991526@gmail.com>
2021-09-09 13:32:51 +0800

    Add

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ export web_az=$(curl http://169.254.169.254/latest/meta-data/placement/availabil
 echo $instantId >> a.log
 echo $web_az >> a.log
 npm install
-npm start > nohup.out &
+nohup npm start > nohup.out &
