de81ca57c820ddcd03c699ac41d0b76b63b42638
Robin <robin1991526@gmail.com>
2021-09-09 11:47:12 +0800

    Add

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-export instantId=$(curl http://169.254.169.254/latest/meta-data/instance-id) && web_az=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone) && npm install &&  npm start
+instantId=$(curl http://169.254.169.254/latest/meta-data/instance-id) && web_az=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone) && npm install &&  npm start
