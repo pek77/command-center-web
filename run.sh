110ed779f0879657c7773fa5e94f1c8036e1fefb
Robin <robin1991526@gmail.com>
2021-09-09 11:56:16 +0800

    Add

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export instantId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 export web_az=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
 npm install
-npm start
+nohup npm start > nohup.out &
