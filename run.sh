b7b7388258a3f83e66415a21c25ef8b13409aac9
Robin <robin1991526@gmail.com>
2021-09-09 11:47:06 +0800

    Add

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-export web_az=EA-sa && export instantId=111 && npm install && nohup npm start > nohup.out
+export instantId=$(curl http://169.254.169.254/latest/meta-data/instance-id) && web_az=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone) && npm install &&  npm start
